e94a56e72594fc99cdfc11c3ea7aa15c0a7efdd7
tim <apkapb@gmail.com>
2026-04-07 13:24:21 +0800

    test: add cases for removing self-defending wrappers and debug-protection variants

--- web/packages/js-deob/src/deobfuscate/self-defending.ts
+++ web/packages/js-deob/src/deobfuscate/self-defending.ts
@@ -1,14 +1,8 @@
 import type { NodePath } from '@babel/traverse'
-import type * as t from '@babel/types'
+import * as t from '@babel/types'
 import type { Transform } from '../ast-utils'
 import * as m from '@codemod/matchers'
-import {
-  constMemberExpression,
-  falseMatcher,
-  findParent,
-  iife,
-  trueMatcher,
-} from '../ast-utils'
+import { findParent, iife } from '../ast-utils'
 
 // SingleCallController: https://github.com/javascript-obfuscator/javascript-obfuscator/blob/d7f73935557b2cd15a2f7cd0b01017d9cddbd015/src/custom-code-helpers/common/templates/SingleCallControllerTemplate.ts
 
@@ -23,122 +17,27 @@ export default {
   tags: ['safe'],
   scope: true,
   visitor() {
-    const callController = m.capture(m.anyString())
-    const firstCall = m.capture(m.identifier())
-    const rfn = m.capture(m.identifier())
-    const context = m.capture(m.identifier())
-    const res = m.capture(m.identifier())
-    const fn = m.capture(m.identifier())
-
-    // const callControllerFunctionName = (function() { ... })();
-    const matcher = m.variableDeclarator(
-      m.identifier(callController),
-      iife(
-        [],
-        m.blockStatement([
-          // let firstCall = true;
-          m.variableDeclaration(undefined, [
-            m.variableDeclarator(firstCall, trueMatcher),
-          ]),
-          // return function (context, fn) {
-          m.returnStatement(
-            m.functionExpression(
-              null,
-              [context, fn],
-              m.blockStatement([
-                m.variableDeclaration(undefined, [
-                  // const rfn = firstCall ? function() {
-                  m.variableDeclarator(
-                    rfn,
-                    m.conditionalExpression(
-                      m.fromCapture(firstCall),
-                      m.functionExpression(
-                        null,
-                        [],
-                        m.blockStatement([
-                          // if (fn) {
-                          m.ifStatement(
-                            m.fromCapture(fn),
-                            m.blockStatement([
-                              // const res = fn.apply(context, arguments);
-                              m.variableDeclaration(undefined, [
-                                m.variableDeclarator(
-                                  res,
-                                  m.callExpression(
-                                    constMemberExpression(
-                                      m.fromCapture(fn),
-                                      'apply',
-                                    ),
-                                    [
-                                      m.fromCapture(context),
-                                      m.identifier('arguments'),
-                                    ],
-                                  ),
-                                ),
-                              ]),
-                              // fn = null;
-                              m.expressionStatement(
-                                m.assignmentExpression(
-                                  '=',
-                                  m.fromCapture(fn),
-                                  m.nullLiteral(),
-                                ),
-                              ),
-                              // return res;
-                              m.returnStatement(m.fromCapture(res)),
-                            ]),
-                          ),
-                        ]),
-                      ),
-                      // : function() {}
-                      m.functionExpression(null, [], m.blockStatement([])),
-                    ),
-                  ),
-                ]),
-                // firstCall = false;
-                m.expressionStatement(
-                  m.assignmentExpression(
-                    '=',
-                    m.fromCapture(firstCall),
-                    falseMatcher,
-                  ),
-                ),
-                // return rfn;
-                m.returnStatement(m.fromCapture(rfn)),
-              ]),
-            ),
-          ),
-        ]),
-      ),
-    )
-
     const emptyIife = iife([], m.blockStatement([]))
 
     return {
       VariableDeclarator(path) {
-        if (!matcher.match(path.node)) return
-        const binding = path.scope.getBinding(callController.current!)
+        const match = matchSelfDefendingController(path.node)
+        if (!match) return
+
+        const binding = path.scope.getBinding(match.callControllerName)
         if (!binding) return
-        // const callControllerFunctionName = (function() { ... })();
-        //       ^ path/binding
 
         binding.referencePaths
           .filter(ref => ref.parent.type === 'CallExpression')
           .forEach((ref) => {
             if (ref.parentPath?.parent.type === 'CallExpression') {
-              // callControllerFunctionName(this, function () { ... })();
-              // ^ ref
               ref.parentPath.parentPath?.remove()
             }
             else {
-              // const selfDefendingFunctionName = callControllerFunctionName(this, function () {
-              // selfDefendingFunctionName();      ^ ref
               removeSelfDefendingRefs(ref as NodePath<t.Identifier>)
             }
 
-            // leftover (function () {})() from debug protection function call
             findParent(ref, emptyIife)?.remove()
-
             this.changes++
           })
 
@@ -149,11 +48,154 @@ export default {
   },
 } satisfies Transform
 
+function isApplyLikeCall(node: t.Node | null | undefined, fnName: string, contextName: string) {
+  if (!t.isCallExpression(node)) return false
+  if (!t.isMemberExpression(node.callee)) return false
+  if (!t.isIdentifier(node.callee.object, { name: fnName })) return false
+  if (node.arguments.length !== 2) return false
+
+  const [contextArg, argumentsArg] = node.arguments
+  return t.isIdentifier(contextArg, { name: contextName })
+    && t.isIdentifier(argumentsArg, { name: 'arguments' })
+}
+
+function isTrueLike(node: t.Node | null | undefined) {
+  return t.isBooleanLiteral(node, { value: true })
+    || (t.isUnaryExpression(node, { operator: '!' }) && t.isNumericLiteral(node.argument, { value: 0 }))
+}
+
+function isFalseLike(node: t.Node | null | undefined) {
+  return t.isBooleanLiteral(node, { value: false })
+    || (t.isUnaryExpression(node, { operator: '!' }) && t.isArrayExpression(node.argument) && node.argument.elements.length === 0)
+}
+
+function isEmptyFunction(node: t.Node | null | undefined) {
+  return t.isFunctionExpression(node)
+    && node.params.length === 0
+    && node.body.body.length === 0
+}
+
+function hasApplySequence(
+  statements: t.Statement[],
+  fnName: string,
+  contextName: string,
+) {
+  if (statements.length !== 3) return false
+
+  const [resultDeclaration, nullAssignment, returnResult] = statements
+  if (!t.isVariableDeclaration(resultDeclaration) || resultDeclaration.declarations.length !== 1) return false
+  const [resultDeclarator] = resultDeclaration.declarations
+  if (!t.isIdentifier(resultDeclarator.id) || !isApplyLikeCall(resultDeclarator.init, fnName, contextName)) return false
+
+  if (!t.isExpressionStatement(nullAssignment) || !t.isAssignmentExpression(nullAssignment.expression, { operator: '=' })) return false
+  if (!t.isIdentifier(nullAssignment.expression.left, { name: fnName }) || !t.isNullLiteral(nullAssignment.expression.right)) return false
+
+  return t.isReturnStatement(returnResult)
+    && t.isIdentifier(returnResult.argument, { name: resultDeclarator.id.name })
+}
+
+function findGuardedApplySequence(
+  statements: t.Statement[],
+  fnName: string,
+  contextName: string,
+) {
+  for (const statement of statements) {
+    if (!t.isIfStatement(statement) || !t.isIdentifier(statement.test, { name: fnName })) {
+      continue
+    }
+
+    if (!t.isBlockStatement(statement.consequent)) {
+      return false
+    }
+
+    if (hasApplySequence(statement.consequent.body, fnName, contextName)) {
+      return true
+    }
+
+    for (const nestedStatement of statement.consequent.body) {
+      if (!t.isIfStatement(nestedStatement)) continue
+
+      if (t.isBlockStatement(nestedStatement.consequent)
+        && hasApplySequence(nestedStatement.consequent.body, fnName, contextName)) {
+        return true
+      }
+
+      if (t.isBlockStatement(nestedStatement.alternate)
+        && hasApplySequence(nestedStatement.alternate.body, fnName, contextName)) {
+        return true
+      }
+    }
+
+    return false
+  }
+
+  return false
+}
+
+function matchSelfDefendingController(node: t.VariableDeclarator) {
+  if (!t.isIdentifier(node.id)) return null
+  if (!t.isCallExpression(node.init) || node.init.arguments.length !== 0) return null
+
+  const callee = node.init.callee
+  if (!t.isFunctionExpression(callee) && !t.isArrowFunctionExpression(callee)) return null
+  if (!t.isBlockStatement(callee.body) || callee.body.body.length < 2) return null
+
+  const returnControllerStatement = callee.body.body.at(-1)
+  if (!t.isReturnStatement(returnControllerStatement) || !t.isFunctionExpression(returnControllerStatement.argument)) return null
+
+  const firstCallDeclarator = callee.body.body
+    .slice(0, -1)
+    .flatMap(statement => t.isVariableDeclaration(statement) ? statement.declarations : [])
+    .find(declarator => t.isIdentifier(declarator.id) && isTrueLike(declarator.init))
+  if (!firstCallDeclarator || !t.isIdentifier(firstCallDeclarator.id)) return null
+  const firstCallName = firstCallDeclarator.id.name
+
+  const controller = returnControllerStatement.argument
+  if (controller.params.length !== 2) return null
+  const [contextParam, fnParam] = controller.params
+  if (!t.isIdentifier(contextParam) || !t.isIdentifier(fnParam)) return null
+  if (controller.body.body.length < 3) return null
+
+  const returnWrapped = controller.body.body.at(-1)
+  if (!t.isReturnStatement(returnWrapped) || !t.isIdentifier(returnWrapped.argument)) return null
+  const wrappedName = returnWrapped.argument.name
+
+  const flipFirstCall = controller.body.body
+    .slice(0, -1)
+    .findLast(statement =>
+      t.isExpressionStatement(statement)
+      && t.isAssignmentExpression(statement.expression, { operator: '=' })
+      && t.isIdentifier(statement.expression.left, { name: firstCallName })
+      && isFalseLike(statement.expression.right),
+    )
+  if (!flipFirstCall || !t.isExpressionStatement(flipFirstCall)) return null
+
+  const wrappedDeclarator = controller.body.body
+    .slice(0, controller.body.body.indexOf(flipFirstCall))
+    .flatMap(statement => t.isVariableDeclaration(statement) ? statement.declarations : [])
+    .find(declarator =>
+      t.isIdentifier(declarator.id, { name: wrappedName })
+      && t.isConditionalExpression(declarator.init),
+    )
+  if (!wrappedDeclarator || !t.isIdentifier(wrappedDeclarator.id) || !t.isConditionalExpression(wrappedDeclarator.init)) return null
+
+  const wrappedConditional = wrappedDeclarator.init
+  if (!t.isIdentifier(wrappedConditional.test, { name: firstCallName })) return null
+  if (!t.isFunctionExpression(wrappedConditional.consequent) || !isEmptyFunction(wrappedConditional.alternate)) return null
+
+  const wrappedBody = wrappedConditional.consequent.body.body
+  if (!findGuardedApplySequence(wrappedBody, fnParam.name, contextParam.name)) return null
+
+  return {
+    callControllerName: node.id.name,
+  }
+}
+
 function removeSelfDefendingRefs(path: NodePath<t.Identifier>) {
   const varName = m.capture(m.anyString())
   const varMatcher = m.variableDeclarator(
     m.identifier(varName),
-    m.callExpression(m.identifier(path.node.name)),
+    m.callExpression(m.identifier(path.node.name), m.arrayOf(m.anyExpression())),
   )
   const callMatcher = m.expressionStatement(
     m.callExpression(m.identifier(m.fromCapture(varName)), []),
@@ -164,8 +206,9 @@ function removeSelfDefendingRefs(path: NodePath<t.Identifier>) {
     const binding = varDecl.scope.getBinding(varName.current!)
 
     binding?.referencePaths.forEach((ref) => {
-      if (callMatcher.match(ref.parentPath?.parent))
+      if (callMatcher.match(ref.parentPath?.parent)) {
         ref.parentPath?.parentPath?.remove()
+      }
     })
     varDecl.remove()
   }
--- web/packages/js-deob/src/test/deob.test.ts
+++ web/packages/js-deob/src/test/deob.test.ts
@@ -107,3 +107,196 @@ test('pipeline supports module syntax when evaluating decoder setup code', async
   expect(code).toMatch(/title: ['"]decoded value['"]/) 
   expect(code).not.toContain('decoder(1)')
 })
+
+test('pipeline removes debug-protection variants with setup declarations', async () => {
+  const { code } = await deob(`
+    (function () {
+      var root = typeof window !== 'undefined' ? window : this;
+      root.setInterval(protect, 2000);
+    })();
+
+    function protect(flag) {
+      var dict = {
+        eq: function (left, right) {
+          return left === right;
+        },
+        text: 'string',
+        join: function (left, right) {
+          return left + right;
+        },
+      };
+
+      function inner(value) {
+        if (dict.eq(typeof value, dict.text)) {
+          function loop() {
+            while (true) {}
+          }
+
+          return loop();
+        } else if (dict.join('', value / value).length !== 1 || value % 20 === 0) {
+          debugger;
+        } else {
+          debugger;
+        }
+
+        inner(++value);
+      }
+
+      try {
+        if (flag) {
+          return inner;
+        } else {
+          inner(0);
+        }
+      } catch (error) {}
+    }
+  `)
+
+  expect(code).not.toContain('setInterval(protect, 2000)')
+  expect(code).not.toContain('function protect')
+  expect(code).not.toContain('function inner')
+  expect(code).not.toContain('debugger')
+})
+
+test('pipeline decodes variable-declared string-array decoders', async () => {
+  const { code } = await deob(`
+    function arrWrap() {
+      var arr = ['alpha', 'beta'];
+      arrWrap = function () {
+        return arr;
+      };
+      return arrWrap();
+    }
+
+    (function (source, seed) {
+      return source;
+    })(arrWrap, 0x10);
+
+    var decoder = function (index, key) {
+      var values = arrWrap();
+      return values[index];
+    };
+
+    console.log(window[decoder(1, 'salt')]);
+  `)
+
+  expect(code).toContain('window.beta')
+  expect(code).not.toContain("decoder(1, 'salt')")
+})
+
+test('pipeline removes self-defending wrappers with encoded apply access', async () => {
+  const { code } = await deob(`
+    var once = function () {
+      var first = true;
+
+      return function (context, fn) {
+        var wrapped = first
+          ? function () {
+              if (fn) {
+                var result = fn[_0xdeadbe(2817, '1MVw')](context, arguments);
+                fn = null;
+                return result;
+              }
+            }
+          : function () {};
+
+        first = false;
+        return wrapped;
+      };
+    }();
+
+    var guarded = once(this, function () {
+      return 'kept';
+    });
+
+    guarded();
+    console.log('done');
+  `)
+
+  expect(code).toContain('console.log("done")')
+  expect(code).not.toContain('var once')
+  expect(code).not.toContain('guarded()')
+  expect(code).not.toContain('_0xdeadbe')
+})
+
+test('pipeline removes self-defending wrappers with noisy encoded apply branches', async () => {
+  const { code } = await deob(`
+    var once = function () {
+      var first = true;
+
+      return function (context, fn) {
+        var wrapped = first
+          ? function () {
+              var cache = { unused: true };
+
+              if (fn) {
+                if (cache.unused === false) {
+                  console.log('noise');
+                } else {
+                  var result = fn[_0xfeedaa(3532, 'N(*o')](context, arguments);
+                  fn = null;
+                  return result;
+                }
+              }
+            }
+          : function () {};
+
+        first = false;
+        return wrapped;
+      };
+    }();
+
+    var guarded = once(this, function () {
+      return 'kept';
+    });
+
+    guarded();
+    console.log('done');
+  `)
+
+  expect(code).toContain('console.log("done")')
+  expect(code).not.toContain('var once')
+  expect(code).not.toContain('guarded()')
+  expect(code).not.toContain('_0xfeedaa')
+})
+
+test('pipeline removes self-defending wrappers with setup declarations', async () => {
+  const { code } = await deob(`
+    var once = function () {
+      var outerState = { mode: 'noop' };
+      var first = true;
+
+      return function (context, fn) {
+        var localState = { enabled: true };
+        var wrapped = first
+          ? function () {
+              if (fn) {
+                if (localState.enabled === false) {
+                  console.log(outerState.mode);
+                } else {
+                  var result = fn[_0xbeef01(3532, 'N(*o')](context, arguments);
+                  fn = null;
+                  return result;
+                }
+              }
+            }
+          : function () {};
+
+        first = false;
+        return wrapped;
+      };
+    }();
+
+    var guarded = once(this, function () {
+      return 'kept';
+    });
+
+    guarded();
+    console.log('done');
+  `)
+
+  expect(code).toContain('console.log("done")')
+  expect(code).not.toContain('var once')
+  expect(code).not.toContain('guarded()')
+  expect(code).not.toContain('_0xbeef01')
+})
